c8c4afc1f699e81808a69a45a3f56a7f7c28f0a9
Ansel Joseph <anseljoseph2509@gmail.com>
2025-09-04 15:05:42 +0530

    add: various colours onclick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,8 @@
-import {MouseEvent} from "react";
+import { useState } from "react";
 
 function ListGroup() {
     let items = ['New York', 'Chicago', 'Delhi', 'Tokyo', 'London'];
-
-    //Event Handler
-    const handleClick = (event : MouseEvent) => console.log(event);
+    const [selectedIndex, setSelectedIndex]= useState(-1);
 
     return (
         <>
@@ -12,7 +10,7 @@ function ListGroup() {
 
             {items.length === 0 && <p>No Items found</p>}
             <ul className="list-group">
-                {items.map((item, index) => (<li className="list-group-item" key={item} onClick={handleClick}>{item}</li>))}
+                {items.map((item, index) => (<li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key={item} onClick={() =>{setSelectedIndex (index);}}>{item}</li>))}
 
             </ul>
         </>
